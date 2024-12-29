fbd0ceb34a80ca934a0d72b3f41fd12dc910ab24
lucasdeprit <lucasdeprit@gmail.com>
2024-12-29 18:50:37 +0100

    struct examples added && stock management exersize

---
+++ structs/product_management/src/main.rs
@@ -0,0 +1,65 @@
+/*
+Product Management
+Create a struct Product to represent a product with a name, price, and stock quantity. Then, create a struct Inventory to manage multiple products.
+
+Input Example:
+
+rust
+Copy code
+let mut inventory = Inventory::new();
+inventory.add_product("Apple", 1.0, 10);
+inventory.add_product("Banana", 0.5, 20);
+println!("{}", inventory.total_value()); // 20.0
+inventory.update_stock("Apple", 15);
+println!("{}", inventory.total_value()); // 22.5
+*/
+
+struct Product {
+    name: String,
+    price: u64, // cents
+    stock: u64,
+}
+
+struct Inventory {
+    products: Vec<Product>,
+}
+
+impl Inventory {
+    fn new() -> Self {
+        Inventory {
+            products: Vec::new(),
+        }
+    }
+
+    fn add_product(&mut self, name: &str, price: u64, stock: u64) {
+        self.products.push(Product {
+            name: String::from(name),
+            price,
+            stock,
+        });
+    }
+
+    fn update_stock(&mut self, name: &str, new_stock: u64) {
+        if let Some(product) = self.products.iter_mut().find(|x| x.name == name) {
+            product.stock = new_stock;
+        }
+    }
+
+    fn total_value(&self) -> u64 {
+        let mut response: u64 = 0;
+        for product in self.products.iter() {
+            response += product.price * product.stock;
+        }
+        response
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+    let mut inventory = Inventory::new();
+    inventory.add_product("Apple", 100, 10);
+    inventory.add_product("Banana", 50, 20);
+    println!("{}", inventory.total_value()); // 20.0
+    inventory.update_stock("Apple", 15);
+    println!("{}", inventory.total_value()); // 22.5
+}
---
+++ structs/word_counter/src/main.rs
@@ -0,0 +1,30 @@
+/*
+Word Counter
+Create a struct WordCounter that tracks the number of times each word appears in a given text.
+
+Input Example:
+
+rust
+Copy code
+let mut counter = WordCounter::new();
+counter.process_text("hello world hello rust");
+println!("{}", counter.count("hello")); // 2
+println!("{}", counter.count("world")); // 1
+*/
+
+struct WordCounter {
+    process_text: String,
+}
+
+impl WordCounter {
+    fn count(text: String) {
+        self.process_text.contains
+    }
+}
+
+fn main() {
+    let mut counter = WordCounter;
+
+    counter.process_text("");
+    println!("Hello, world!");
+}
