bdfb10114872526de94d307d9b91f3d49ee27145
lucasdeprit <lucasdeprit@gmail.com>
2025-01-03 11:58:36 +0100

    count_fruits example added

---
+++ enums_pattern_matching/count_fruits/src/main.rs
@@ -0,0 +1,49 @@
+/*
+4. [Medium-Hard] Count Fruits
+Create an enum Fruit with the following variants:
+
+Apple
+Banana
+Cherry
+Write a function that takes a vector of Fruit and counts how many of each type are present, returning the counts in a tuple (apples, bananas, cherries).
+
+Input:
+count_fruits(vec![Fruit::Apple, Fruit::Banana, Fruit::Cherry, Fruit::Apple])
+
+Output:
+(2, 1, 1)
+
+*/
+
+enum Fruit {
+    Apple,
+    Banana,
+    Cherry,
+}
+
+fn count_fruits(fruits: Vec<Fruit>) -> (i32, i32, i32) {
+    let mut apples = 0;
+    let mut bananas = 0;
+    let mut cherries = 0;
+
+    for fruit in fruits {
+        match fruit {
+            Fruit::Apple => apples += 1,
+            Fruit::Banana => bananas += 1,
+            Fruit::Cherry => cherries += 1,
+        }
+    }
+
+    (apples, bananas, cherries)
+}
+
+fn main() {
+    let (x, y, z) = count_fruits(vec![
+        Fruit::Apple,
+        Fruit::Banana,
+        Fruit::Cherry,
+        Fruit::Apple,
+    ]);
+
+    println!("({}, {}, {})", x, y, z)
+}
